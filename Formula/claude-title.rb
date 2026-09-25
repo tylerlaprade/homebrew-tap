f37d84341367ff76d4c44cff2e4c897a326f9fc2
Tyler Laprade, CFA <pinkmonkeyblue@gmail.com>
2026-09-25 17:27:38 -0400

    claude-title 0.1.9

--- Formula/claude-title.rb
+++ Formula/claude-title.rb
@@ -1,21 +1,21 @@
 class ClaudeTitle < Formula
   desc "Show Claude Code's status in the terminal tab title."
   homepage "https://github.com/tylerlaprade/claude-title"
-  version "0.1.8"
+  version "0.1.9"
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/tylerlaprade/claude-title/releases/download/v0.1.8/claude-title-aarch64-apple-darwin.tar.xz"
-      sha256 "c312145dd0f5cfc6c2f9ac1e936a984b164077e2fb36c07173e0638f43568507"
+      url "https://github.com/tylerlaprade/claude-title/releases/download/v0.1.9/claude-title-aarch64-apple-darwin.tar.xz"
+      sha256 "dd2d1c6a7e2fb21051c35a817c6938266b731b54ada83a136580ed4dfededbd6"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/tylerlaprade/claude-title/releases/download/v0.1.8/claude-title-x86_64-apple-darwin.tar.xz"
-      sha256 "a20da572e2ede0b3dfe65211f690147cbbcd4004a48652e57de48ed1ac73d8a2"
+      url "https://github.com/tylerlaprade/claude-title/releases/download/v0.1.9/claude-title-x86_64-apple-darwin.tar.xz"
+      sha256 "94723b88436a64d2d06697e79ffb7a1650c8e90ae484d94ccbfce17898f7b7c0"
     end
   end
   if OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/tylerlaprade/claude-title/releases/download/v0.1.8/claude-title-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "6e2107e392bfddcf1b15bac32be4028d4c0ff1e883ea8288e2e8293aa7195bf9"
+      url "https://github.com/tylerlaprade/claude-title/releases/download/v0.1.9/claude-title-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "08c6db77a252d41c8a13ce950819f9342dd7121b10dd7e5d3927ca7de3478718"
     end
   end
   license "GPL-3.0-only"
